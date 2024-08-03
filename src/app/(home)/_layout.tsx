c18e4c3ff56fe0492e64b419f69feec69c6eedf4
emirkymz <16emirkymz@gmail.com>
2024-08-03 19:25:59 +0300

    Stream Chat Provider

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,36 +1,13 @@
-import { Slot, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat, OverlayProvider } from 'stream-chat-expo';
-
-const client = StreamChat.getInstance('');
+import ChatProvider from '@/src/providers/ChatProvider';
+import { Stack } from 'expo-router';
 
 export default function homeLayout() {
 
-    useEffect(() => {
-        const connect = async () => {
-            await client.connectUser({
-                id: 'user-id1',
-                name: 'user-name1',
-            },
-            client.devToken('user-id1')
-            );
-
-            /* const channel = client.channel('messaging', 'channel-id1', {
-                name: 'channel-name1',
-            });
-            await channel.watch(); */
-        };
-        connect();
-    });
-
     return(
-        <OverlayProvider>
-            <Chat client={client}> 
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                </Stack>
-            </Chat>
-        </OverlayProvider>
+        <ChatProvider>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </ChatProvider>
     );
 }
