ee06d11e75388ed9ee2b90159454b276d03d3773
emirkymz <16emirkymz@gmail.com>
2024-08-04 13:38:28 +0300

    Users List

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,16 +1,27 @@
 import { Text } from 'react-native';
 import { ChannelList } from 'stream-chat-expo';
-import { router } from 'expo-router';
+import { Link, Stack, router } from 'expo-router';
 import { useAuth } from '../../../providers/AuthProvider';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function MainTabScreen() {
 
     const { user } = useAuth();
 
     return ( 
-        <ChannelList 
-            filters={{members:{ $in: [user.id]}}}
-            onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
-        />
+        <>
+            <Stack.Screen options={{
+                headerRight: () => (
+                    <Link href={`/(home)/users`} asChild>
+                    <AntDesign name="pluscircle" size={24} color="gray" style={{marginHorizontal: 16}} />
+                    </Link>
+                    ),
+                }}
+            />
+            <ChannelList 
+                filters={{members:{ $in: [user.id]}}}
+                onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+            />
+        </>
     );
 }
